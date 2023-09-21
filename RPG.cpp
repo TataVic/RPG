5adc0e7a54d94d858e98c7447746074dc3d26cb0
Tauani Vitória <tauanivitoria1302@gmail.com>
2023-09-20 22:01:22 -0300

    Código Bia
    Introdução das classes e seus devidos atributos.

--- RPG.cpp
+++ RPG.cpp
@@ -1,1 +1,55 @@
-#teste
+#include<iostream>
+using namespace std;
+#include <string>;
+
+class personagem{
+ string nome;
+ int pv, pa,pd;
+  personagem **derrotados;
+
+
+
+};
+
+class JogoRPG{
+
+
+};
+
+class bruxo : personagem{
+  int atck;
+  
+};
+
+class mago : personagem{
+ int atck;
+};
+
+class guerreiro : personagem{
+ int atck;
+};
+
+class druida: personagem{
+ int atck;
+};
+
+class clerigo: personagem{
+ int atck;
+};
+
+class bandido: personagem{
+ int atck;
+};
+
+
+
+/*mais seis classe distintas dos arquetipos que são derivadas do personagem
+Pontos de vida (VID).
+Pontos de ataque (ATQ).
+Pontos de defesa (DEF).
+uma lista de struct que puxa as classes personagens
+o pai vai ser o personagem e o filho vai ser o arquetipo um vetor de ponteiros*/
+
+int main(){
+
+}
